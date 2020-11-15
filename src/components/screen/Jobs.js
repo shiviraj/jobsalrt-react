017f64a50ced526c0a91dd49572f4bb904435ee8
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-11-15 14:29:05 +0530

    |#9|Modified jobs page

--- src/components/screen/Jobs.js
+++ src/components/screen/Jobs.js
@@ -1,44 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import Loader from '../includes/Loader';
-import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 import fetchApi from '../../api/fetchApi';
 
-const Section = styled.section`
-  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.4);
-  border: 1px solid #0c4da2;
-  margin: 32px;
-  @media only screen and (max-width: 420px) {
-    & {
-      margin: 8px 4px;
-    }
-  }
-`;
+import { Typography, Grid, Paper, ListItem } from '@material-ui/core';
+import { List, Link } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 
-const Title = styled.h1`
-  color: #fff;
-  background: #0c4da2ef;
-  padding: 4px 32px;
-  font-weight: 500;
-  border: 1px solid #0c4da2;
-`;
-
-const List = styled.ul`
-  padding: 4px 32px;
-  background: #efefef;
-`;
-const ListItem = styled.li`
-  padding: 4px;
-`;
-
-const Link = styled.a`
-  text-decoration: none;
-  color: #0c4da2;
-  &:hover {
-    text-decoration: underline;
-  }
-`;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+  paper: {
+    border: '1px solid #0c4da2',
+    boxShadow: '4px 4px 4px rgba(0, 0, 0, 0.4)',
+    color: theme.palette.text.secondary,
+  },
+  heading: {
+    color: '#fff',
+    background: theme.palette.primary.main,
+    padding: ' 4px',
+    fontSize: ' 20px',
+    fontWeight: ' 500',
+    border: `1px solid ${theme.palette.primary.main}`,
+  },
+  list: {
+    padding: theme.spacing(1) / 10,
+  },
+  listItem: {
+    padding: theme.spacing(1) / 2.5,
+    fontWeight: '200',
+    '&:hover': {
+      background: theme.palette.grey['300'],
+    },
+  },
+}));
 
 const AllJobs = (props) => {
+  const classes = useStyles();
   const type = props.location.pathname.slice(1).replace('-', '_').toUpperCase();
   const [posts, setPosts] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -53,22 +52,38 @@ const AllJobs = (props) => {
   }, [type]);
 
   return (
-    <Section>
-      <Title>{type.replace('_', ' ')}</Title>
-      <List>
-        {isLoading ? (
-          <Loader />
-        ) : (
-          posts.map((post) => {
-            return (
-              <ListItem key={post._id}>
-                <Link href={`/post/${post.url}`}>{post.title}</Link>
-              </ListItem>
-            );
-          })
-        )}
-      </List>
-    </Section>
+    <div className={classes.root}>
+      <Grid item sm={12}>
+        <Paper className={classes.paper}>
+          <Typography variant="h3" className={classes.heading}>
+            {type.replace('_', ' ')}
+          </Typography>
+          <List component="ul" aria-label="posts" className={classes.list}>
+            {isLoading || !posts ? (
+              <Loader />
+            ) : (
+              posts.map((post) => {
+                return (
+                  <Link
+                    component={NavLink}
+                    to={`/post/${post.url}`}
+                    color="primary"
+                  >
+                    <ListItem
+                      key={post._id}
+                      className={classes.listItem}
+                      component="li"
+                    >
+                      ⁕ {post.title}
+                    </ListItem>
+                  </Link>
+                );
+              })
+            )}
+          </List>
+        </Paper>
+      </Grid>
+    </div>
   );
 };
 
