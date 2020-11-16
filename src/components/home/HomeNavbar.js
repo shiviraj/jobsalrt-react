7bff1406e62c8c93f6848179415a4d0cf8b5587e
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-11-16 17:29:39 +0530

    |#9|Added tab menu in home page

--- src/components/home/HomeNavbar.js
+++ src/components/home/HomeNavbar.js
@@ -1,75 +1,87 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
 import category from '../../media/icons/category.png';
 import location from '../../media/icons/location.png';
 import company from '../../media/icons/company.png';
 import qualification from '../../media/icons/qualification.png';
+import { makeStyles } from '@material-ui/core/styles';
+import { Tabs, Paper, Tab } from '@material-ui/core';
 
-const NavLayout = styled.nav`
-  display: flex;
-  border-top: 1px solid #ccc;
-  border-bottom: 1px solid #0c4da2;
-  background: #eee;
-  padding: 0 16px;
-  margin-top: 32px;
-  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.4);
-  & a {
-    text-decoration: none;
-    padding: 8px 32px;
-    color: #0c4da2;
-    display: flex;
-    align-items: center;
-  }
-  & a img {
-    display: none;
-    height: 32px;
-  }
-  & .active {
-    border: 1px solid #0c4da2;
-    background: #ccc;
-    border-bottom: 1px solid #ccc;
-    margin-bottom: -1px;
-  }
-  @media only screen and (max-width: 420px) {
-    & {
-      padding: 0 4px;
-      margin-top: 4px;
-      font-size: 14px;
-    }
-    & a {
-      padding: 2px 8px;
-    }
-    & a img {
-      display: block;
-    }
-    & a span {
-      display: none;
-    }
-  }
-`;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '0px',
+    flexGrow: 1,
+    display: 'block',
+    [`@media only screen and (max-width: 420px)`]: {
+      display: 'none',
+      height: '0px',
+    },
+  },
+  smallView: {
+    display: 'none',
 
-const HomeNavBar = () => {
+    '& img': {
+      height: '32px',
+    },
+    [`@media only screen and (max-width: 420px)`]: {
+      display: 'block',
+    },
+  },
+}));
+
+const HomeNavbar = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <NavLayout>
-      <NavLink to="/home" activeClassName="active" exact>
-        <img src={category} alt="Category" />
-        <span>Category Wise</span>
-      </NavLink>
-      <NavLink to="/home/location">
-        <img src={location} alt="Location" />
-        <span>Location Wise</span>
-      </NavLink>
-      <NavLink to="/home/company">
-        <img src={company} alt="Company" />
-        <span>Department / Company Wise</span>
-      </NavLink>
-      <NavLink to="/home/qualification">
-        <img src={qualification} alt="Qualification" />
-        <span>Qualification Wise</span>
-      </NavLink>
-    </NavLayout>
+    <>
+      <Paper square className={classes.smallView}>
+        <Tabs value={value} onChange={handleChange} indicatorColor="Primary">
+          <Tab
+            component={NavLink}
+            to="/home"
+            icon={<img src={category} alt="category" />}
+          />
+          <Tab
+            component={NavLink}
+            to="/home/location"
+            icon={<img src={location} alt="location" />}
+          />
+          <Tab
+            component={NavLink}
+            to="/home/company"
+            icon={<img src={company} alt="company" />}
+          />
+          <Tab
+            component={NavLink}
+            to="/home/qualification"
+            icon={<img src={qualification} alt="qualification" />}
+          />
+        </Tabs>
+      </Paper>
+      <Paper square className={classes.root}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="Primary"
+          textColor="primary"
+        >
+          <Tab component={NavLink} to="/home" label="Category" />
+          <Tab component={NavLink} to="/home/location" label="Location" />
+          <Tab component={NavLink} to="/home/company" label="Company" />
+          <Tab
+            component={NavLink}
+            to="/home/qualification"
+            label="Qualifications"
+          />
+        </Tabs>
+      </Paper>
+    </>
   );
 };
 
-export default HomeNavBar;
+export default HomeNavbar;
